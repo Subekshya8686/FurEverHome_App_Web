e19f8ba550cf95103d720f0bb04f5202107b6c0a
Subekshya <subekshyakayastha4@gmail.com>
2025-01-05 13:48:40 +0545

    add footer

---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = ({ footerRef }) => {
+  return (
+    <div
+      ref={footerRef}
+      className="py-4 text-center"
+      style={{
+        backgroundColor: "#FCDDC9",
+        // position: "absolute",
+        bottom: 0,
+        width: "100%",
+      }}
+    >
+      <p className="text-sm text-[#A35E47] font-poppins">
+        © 2025 Animal Adoption Platform. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
