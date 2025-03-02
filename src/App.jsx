a4feac39188e9fe4fe1af02f3f48c858b2c7dfd4
Subekshya <subekshyakayastha4@gmail.com>
2025-03-02 14:38:16 +0545

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
+import PetAdoptionForms from "./core/private/forms/AdoptionForms";
+import PetFosterForm from "./core/private/forms/FosterForms";
 import Layout from "./core/private/layout";
 import AdminDashboard from "./core/private/pets/AdminDashboard";
+import AdminProfile from "./core/private/user/UserProfile";
 import UsersTable from "./core/private/user/UsersTable";
 import Dashboard from "./core/public/dashboard";
 import PetAdoptionForm from "./core/public/PetAdoptionForm";
+import FosterApplicationForm from "./core/public/PetFosterForm";
 import PetProfile from "./core/public/PetProfile";
 import SearchPage from "./core/public/SearchPage";
 import UserProfile from "./core/public/UserProfile";
@@ -31,6 +35,9 @@ function App() {
         { path: "/", element: <Navigate to="/dashboard" replace /> },
         { path: "dashboard", element: <AdminDashboard /> },
         { path: "user", element: <UsersTable /> },
+        { path: "adoption-forms", element: <PetAdoptionForms /> },
+        { path: "foster-forms", element: <PetFosterForm /> },
+        { path: "user-profile", element: <AdminProfile /> },
       ],
     },
   ];
@@ -38,6 +45,7 @@ function App() {
   const publicRoutes = [
     { path: "/profile/:id", element: <PetProfile /> },
     { path: "/adoption/:id", element: <PetAdoptionForm /> },
+    { path: "/foster/:id", element: <FosterApplicationForm /> },
     { path: "/user/:id", element: <UserProfile /> },
     { path: "/search/:query?", element: <SearchPage /> },
     { path: "/", element: <Dashboard /> },
@@ -48,7 +56,7 @@ function App() {
 
   // const router = token ? privateRoutes : publicRoutes;
   console.log("Role:", role);
-  const router = role === "User" ? privateRoutes : publicRoutes; // Admin-specific routes
+  const router = role === "Admin" ? privateRoutes : publicRoutes; // Admin-specific routes
 
   const allRoutes = [
     ...router,
